8faab0051ecc1896f22516e3bcace03860785d56
nikhil-cod <nikhilchakole1997@gmail.com>
2023-02-18 21:05:00 +0530

    Prevented Same user

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -3,9 +3,14 @@ const UserSchema = require("../models/user");
 exports.createUser =async (req, resp) => {
   const {name,email,password} = req.body;
 
-  const newUser = new UserSchema({name,email,password});
+  //To check the user is already saved
+  const olduser = await UserSchema.findOne({email});
+  if(olduser) 
+    return resp.status(401).json({"error":"User Already Exists!"});
 
+    //Else Below code will be run id user doesnot exists 
+  const newUser = new UserSchema({name,email,password});
   await newUser.save();
-  resp.json({user:newUser})
+  resp.status(201).json({user:newUser})
 };
 
