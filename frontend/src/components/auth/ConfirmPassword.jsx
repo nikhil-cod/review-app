6f3974cd18be8e725ea0e2a650cec56b776853ed
nikhil-cod <nikhilchakole1997@gmail.com>
2023-07-06 17:41:13 +0530

    click on login on navbar

--- frontend/src/components/auth/ConfirmPassword.jsx
+++ frontend/src/components/auth/ConfirmPassword.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-
+import Container from "../Container";
+import FormInput from "../form/FormInput";
+import Submit from "../form/Submit";
+import Title from "../form/Title";
 
 export default function ConfirmPassword({ children, to }) {
-    return <div>
-       Confirm password
-    </div>
+    return  <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
+    <Container>
+      <form className="bg-secondary rounded p-6 w-96 space-y-6">
+        <Title>Enter New Password</Title>
+        <FormInput label="New Password" placeholder="********" name="password" type="password" />
+        <FormInput label="Confirm Password" placeholder="********" name="confirmPassword" type="password" />
+        <Submit value="Confirm Password" />
+      </form>
+    </Container>
+  </div>
 }
--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BsFillSunFill } from "react-icons/bs";
 import Container from "../Container";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="bg-secondary shadow-sm shadow-gray-500">
       <Container className="p-2">
         <div className="flex justify-between items-center">
-          <img src="./logo.png" alt="" className="h-10" />
+          <Link to={'/'}><img src="./logo.png" alt="" className="h-10" /></Link>
 
           <ul className="flex items-center space-x-4">
             <li>
@@ -22,7 +23,7 @@ export default function Navbar() {
                 placeholder="search..."
               />
             </li>
-            <li className="text-white font-semibold text-lg">Login</li>
+            <Link to={'/auth/signin'}><li className="text-white font-semibold text-lg">Login</li></Link>
           </ul>
         </div>
       </Container>
